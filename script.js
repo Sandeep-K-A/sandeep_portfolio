f2476fdab7052b72db750612b85b626506dff113
Sandeep K A <kasandeep42@gmail.com>
2026-02-18 23:15:19 +0530

    transitions added

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const navToggle = document.getElementById("nav-toggle");
 
-
-navToggle.addEventListener("click",()=>{
-    const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
-    navToggle.setAttribute("aria-expanded",String(!isExpanded));
+navToggle.addEventListener("click", () => {
+  const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
+  navToggle.setAttribute("aria-expanded", String(!isExpanded));
 });
 
-const sections = document.querySelectorAll('.reveal-section');
+const sections = document.querySelectorAll(".reveal-section");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
 
-    const reveals = entry.target.querySelectorAll('.reveal');
+      const reveals = entry.target.querySelectorAll(".reveal");
 
-    reveals.forEach((el, index) => {
-      el.style.transitionDelay = `${index * 120}ms`;
-      el.classList.add('is-visible');
+      reveals.forEach((el, index) => {
+        el.style.transitionDelay = `${index * 120}ms`;
+        el.classList.add("is-visible");
+      });
+
+      observer.unobserve(entry.target);
     });
+  },
+  {
+    threshold: 0.25,
+  },
+);
 
-    observer.unobserve(entry.target);
-  });
-}, {
-  threshold: 0.25
-});
-
-sections.forEach(section => observer.observe(section));
+sections.forEach((section) => observer.observe(section));
